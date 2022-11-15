61766f4a2a0beea8b5e274aae869324c52ca18ac
ashushivshettetcs <118234641+ashushivshettetcs@users.noreply.github.com>
2022-11-15 14:54:17 +0530

    Update get.java

--- src/A/get.java
+++ src/A/get.java
@@ -8,5 +8,6 @@ public static void main(String[] args) {
 	System.setProperty("webdriver.chrome.driver","C:\\Users\\Ashwini Shivshette\\Downloads\\chromedriver_win32\\chromedriver.exe " );
 	WebDriver driver=new ChromeDriver();
 	driver.get("https://www.google.com/");
+	driver.get("https://www.amazon.com/");
 }
 }
